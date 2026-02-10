a3100b0f762ff8e028311070ed0e286e9216eed3
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-02-10 12:59:46 +0300

    finished project

---
+++ eas.js
@@ -0,0 +1,40 @@
+const container = document.getElementById("container");
+
+const button = document.getElementById("button");
+
+
+const createGrid = (gridSize) => {
+    if (gridSize > 100) return;
+
+    container.innerHTML = "";
+
+
+    const total = gridSize * gridSize;
+    const squareSize = 320 / gridSize;
+
+
+    for (let i = 0; i < total; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "pink";
+        });
+
+        container.appendChild(square);
+    }
+};
+
+createGrid(16);
+
+button.addEventListener("click", () => {
+    const gridSize = parseInt(prompt("What is the preferred number of grids?"));
+    if (!isNaN(gridSize))  createGrid(gridSize);
+})
+
+
+
+ 
